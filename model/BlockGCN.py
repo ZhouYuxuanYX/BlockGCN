57610451c0aceea47d8b32d8a73fa4f75b1de12c
Yuxuan Zhou <43881388+ZhouYuxuanYX@users.noreply.github.com>
2024-01-07 15:16:51 +0800

    Update BlockGCN.py

--- model/BlockGCN.py
+++ model/BlockGCN.py
@@ -299,7 +299,23 @@ class TCN_GCN_unit(nn.Module):
             return y
         
         
-
+class TopoTrans(nn.Module):
+    def __init__(self, out_dim):
+        super(TopoTrans, self).__init__()
+        self.relu = nn.ReLU()
+        self.mlp = nn.Linear(64,out_dim)
+        # self.tanh = nn.Tanh
+        # self.pa = nn.Parameter(torch.zeros(1), requires_grad=True)
+        self.bn = nn.BatchNorm1d(out_dim)
+        
+    def forward(self, x):
+        # x = x.repeat(2,1)
+        x = self.mlp(x)
+        #BN
+        x = self.bn(x)
+        x = self.relu(x)
+        
+        return x.unsqueeze(2).unsqueeze(3)
 
 
 class Topo(nn.Module):
